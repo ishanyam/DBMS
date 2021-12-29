efd090bafdd9dca04ec18c2af8f52650c0493722
ishanyam <1168272@mail.ru>
2021-12-29 14:12:04 +0300

    Update Deck.h

--- Task_02/Deck.h
+++ Task_02/Deck.h
@@ -1,22 +1,63 @@
 #include <iostream>
 class Node {
+    int _value;
+    Node* next;
 public:
-    int x;
-    Node* Next;
+    /*!
+    \brief Метод является конструктором с параметрами для класса Node.
+    \param[in] value - значение, которое будет записано в элементе дека.
+    */
+    Node(const int value) { _value = value; next = nullptr; };
+    friend class Deck;
 };
 
 class Deck {
-    Node* Head, * Tail;
+    Node* head, * tail;
     size_t size;
 public:
-    Deck() :Head(nullptr), Tail(nullptr), size(0) {};
-    void pushR(const int x);
-    void pushL(const int x);
-    void popR();
-    void popL();
+    /*!
+    \brief Метод является конструктором по умолчанию для класса Deck.
+    */
+    Deck() :head(nullptr), tail(nullptr), size(0) {};
+    /*!
+    \brief МетодМетод вносит новый элемент в дек справа.
+    \param[in] value - значение, которое будет записано в элементе дека.
+    */
+    void pushR(const int value);
+    /*!
+    \brief Метод вносит новый элемент в дек слева.
+    \param[in] value - значение, которое будет записано в элементе дека.
+    */
+    void pushL(const int value);
+    /*!
+    \brief Метод удаляет из дека элемент справа.
+    */
+    int popR();
+    /*!
+    \brief Метод удаляет из дека элемент слева.
+    */
+    int popL();
+    /*!
+    \brief Метод возвращает количество элементов в деке.
+    */
     size_t getSize();
-    void editL(Deck M, const int x);
-    void editR(Deck M, const int x);
-    int printR();
-    int printL();
+    /*!
+    \brief Метод изменяет элемент дека слева.
+    \param[in] value - значение, которое будет записано в элементе дека.
+    */
+    void editL(const int value);
+    /*!
+    \brief Метод изменяет элемент дека справа.
+    \param[in] value - значение, которое будет записано в элементе дека.
+    */
+    void editR(const int value);
+    /*!
+    \brief Метод выводит на печать элемент дека слева.
+    */
+    int getL();
+    /*!
+    \brief Метод выводит на печать элемент дека справа.
+    */
+    int getR();
+    friend std::ostream& operator<< (std::ostream& out, Deck& deck);
 };
